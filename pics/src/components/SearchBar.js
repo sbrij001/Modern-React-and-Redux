c64ccfcb22c042bc8bf6546b5e149ce448de845a
shivani brijmohan <shivani.brijmohan@gmail.com>
2020-12-05 19:08:23 -0500

    created a react app with a search input

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+  onInputChange(e){
+    console.log(e.target.value);
+  }
+
+  render(){
+
+    return(
+
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange}/>
+          </div>
+        </form>
+      </div>
+
+    )
+
+  }
+}
+export default SearchBar;
